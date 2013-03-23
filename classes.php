c50c6ffa4867ff2bef69c0ad69f7f16eeba09b63
Deepanshu Goyal <deepugoyal2708@gmail.com>
2013-03-23 18:33:37 +0530

    Update user class
    user class is added

--- classes.php
+++ classes.php
@@ -200,4 +200,183 @@ class request{
 		$r_db->query("DELETE FROM request WHERE req_id = ".$this->req_id." AND uid = ".$this->cur_user);
 	}
 }
+
+/* Class for user. */
+class user{
+	private $uid;
+	private $name;
+	private $uname;
+	private $pass;
+	private $email;
+	private $designation;
+	private $contact;
+	private $u_type;
+	private $pic;
+	private $about;
+	private $u_db;
+	private $super_flag;
+	private $user_details;
+	
+	// counting the request and approval status
+	private $request_made;
+	private $request_pending;
+	private $request_approved;
+	private $request_rejected;
+	private $approval_pending;
+	private $approval_rejected;
+	private $approval_approved;
+	private $total_approval;
+
+	
+	public function user($id){
+		$this->u_db = new db();
+		$this->user_details = mysql_fetch_array($this->u_db->query('SELECT * from user_details WHERE uid = '.$id.''));
+		//print_r($this->user_details);
+		//echo $this->user_details['u_type'];
+		if($this->user_details){
+			$this->uid = $this->user_details['uid'];
+			$this->uname = $this->user_details['uname'];
+			$this->email = $this->user_details['email'];
+			$this->designation = $this->user_details['designation'];
+			$this->pic = $this->user_details['pic'];
+			$this->desc = $this->user_details['about'];
+			$this->u_type = $this->user_details['u_type'];
+			$this->contact = $this->user_details['contact'];
+			$this->request_made = 0;
+			$this->request_pending = 0;
+			$this->request_approved = 0;
+			$this->request_rejected = 0;
+			$this->approval_approved = 0;
+			$this->approval_pending = 0;
+			$this->approval_rejected = 0;
+			$this->total_approval = 0;
+			
+			//$this->super_flag = $this->user_details['super_flag'];
+		}	
+		else{
+			//echo error
+			//call destructor?
+			//contructor not fully executed;either fetch error or resource not present; display error message;
+		}	
+	}
+	
+	/* Next two functions to be called from super admin page. */
+	private function add_user(){								//as parameters all the details are to be passed.
+		//$this->u_db->query('-- Insert query here');
+	}
+	private function delete_user(){								//as param the id to be passed.
+		//$this->u_db->query('-- Delete query here');
+	}
+	
+	public function pubAdd_user($isSuper){						//isSuper is a boolean var that will be set by a user class public function $user->issuper() to true if the user is a super admin, false otherwise
+		if($isSuper) $this->add_user();
+		else ;													//echo you don't have the privileges
+	}
+	
+	public function pubDel_user($isSuper){						//isSuper is a boolean var that will be set by a user class public function $user->issuper() to true if the user is a super admin, false otherwise
+		if($isSuper) $this->delete_user();
+		else ;													//echo you don't have the privileges
+	}
+	
+	/* Update/change some personal detail like phone number or email or something. */
+	public function change_detail($detail, $val){
+		//$this->u_db->query('-- update user table set $detail = $val where id = $this->uid');
+	}
+	
+	/* Check if superadmin. */
+	public function issuper(){
+		if($this->super_flag)return true;
+		else return false;
+	}
+	
+	/* Get user details. */
+	public function get_details(){
+		return	$this->user_details;
+	}
+	public function get_profile_data(){
+		while($profile_details = current($this->user_details))
+		{
+			next($this->user_details);
+			if(!(key($this->user_details) == "uid" || key($this->user_details) == "pass" || key($this->user_details) == "u_type" || key($this->user_details) == "about" || key($this->user_details) == "pic"))
+			{
+				if(key($this->user_details) == "uname")
+					$user_type = "username";
+				else $user_type = key($this->user_details);
+				echo "<li><a> ".ucwords($user_type)." : &nbsp".current($this->user_details)." </a></li>";
+			}
+			
+			next($this->user_details);
+		}
+	}
+	
+	// Get about me from user_details
+	public function get_about_me(){
+		echo "<li><a> ".$this->desc." </a></li>";	
+	}
+	
+	// get the user pic otherwise default
+	public function get_pic(){
+		echo $this->pic;
+	}
+	
+	// get the request status i.e. total requests made, requests approved, requests pending.
+	public function get_request_number(){
+		$result = $this->u_db->query('SELECT * from request WHERE uid = '.$this->uid.'');
+		while($row = mysql_fetch_array($result)){
+			if(strtolower($row['status']) == 'pending'){
+				$this->request_made++;
+				$this->request_pending++;		
+			}
+			if(strtolower($row['status']) == 'approved')
+			{
+				$this->request_made++;
+				$this->request_approved++;
+			}
+			if(strtolower($row['status']) == 'rejected')
+			{
+				$this->request_made++;
+				$this->request_rejected++;
+			}
+		}	
+		echo "<li><a> Request made : &nbsp".$this->request_made." </a></li>";
+		echo "<li><a> Request Pending : &nbsp".$this->request_pending." </a></li>";
+		echo "<li><a> Request Approved : &nbsp".$this->request_approved." </a></li>";
+		echo "<li><a> Request Rejected : &nbsp".$this->request_rejected." </a></li>";
+	}
+	
+	// get the approval status if its user type is > 1 i.e. it has the power to approve any request.
+	public function get_approval_number(){
+		if(intval($this->u_type) >= 2)
+		{
+			$result = $this->u_db->query('SELECT * from request_status WHERE uid = '.$this->uid.'');
+			while($row = mysql_fetch_array($result)){
+				
+				if(strtolower($row['status']) == 'pending'){
+					$this->total_approval++;
+					$this->approval_pending++;		
+				}
+				if(strtolower($row['status']) == 'approved')
+				{
+					$this->total_approval++;
+					$this->approval_approved++;
+				}
+				if(strtolower($row['status']) == 'rejected')
+				{
+					$this->total_approval++;
+					$this->approval_rejected++;
+				}
+			}
+			echo "<li><a> Total Approval : &nbsp".$this->total_approval." </a></li>";
+			echo "<li><a> Approval Pending : &nbsp".$this->approval_pending." </a></li>";
+			echo "<li><a> Approval Approved : &nbsp".$this->approval_approved." </a></li>";
+			echo "<li><a> Approval Rejected : &nbsp".$this->approval_rejected." </a></li>";
+			
+		}
+	}
+	
+	// get the  user type
+	public function get_user_type(){
+		return $this->u_type;
+	}
+}
 ?>
